6240a632d33e8d0283634c5a9ae4b1833b25174e
Ubuntu <ubuntu@ip-172-31-66-128.ec2.internal>
2021-09-29 03:21:56 +0000

    updated flask app

--- templates/app.py
+++ templates/app.py
@@ -10,9 +10,11 @@ def home():
 @app.route("/image/", methods = ['GET', 'POST'])
 def generateImage():
     prompt = request.args.get("prompt", default = "student", type = str)
-    fileName = prompt+".png"
-    subprocess.run(["python", "/home/ubuntu/dalle-clip-bias/CLIP+Optim/generate.py", "-p", prompt, "-o", "/home/ubuntu/dalle-clip-bias/templates/static/" + fileName])
-    return render_template("index.html", user_image = fileName)
+    fileName = prompt + ".png"
+    if "_" in prompt:
+        prompt = " ".join(prompt.split("_"))
+    subprocess.run(["python", "/home/ubuntu/dalle-clip-bias/CLIP+Optim/generate.py", "-p", prompt, "-o", "/home/ubuntu/dalle-clip-bias/templates/static/" + fileName, "-i 5"])
+    return render_template("index.html", user_stem = prompt, user_image = fileName)
     
 if __name__ == "__main__":
     app.run(debug=True)
