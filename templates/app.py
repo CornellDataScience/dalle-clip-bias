ec547b5d34ce2eacbe5633be1c3bb83412d228bb
Ubuntu <ubuntu@ip-172-31-66-128.ec2.internal>
2021-09-29 03:23:24 +0000

    removed iteration limit

--- templates/app.py
+++ templates/app.py
@@ -13,7 +13,7 @@ def generateImage():
     fileName = prompt + ".png"
     if "_" in prompt:
         prompt = " ".join(prompt.split("_"))
-    subprocess.run(["python", "/home/ubuntu/dalle-clip-bias/CLIP+Optim/generate.py", "-p", prompt, "-o", "/home/ubuntu/dalle-clip-bias/templates/static/" + fileName, "-i 5"])
+    subprocess.run(["python", "/home/ubuntu/dalle-clip-bias/CLIP+Optim/generate.py", "-p", prompt, "-o", "/home/ubuntu/dalle-clip-bias/templates/static/" + fileName])
     return render_template("index.html", user_stem = prompt, user_image = fileName)
     
 if __name__ == "__main__":
