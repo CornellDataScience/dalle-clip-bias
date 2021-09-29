9c3fb2bb6d523f81e517ba7b7ba8199ee55f9ce0
Ubuntu <ubuntu@ip-172-31-66-128.ec2.internal>
2021-09-29 03:09:23 +0000

    updated api

--- templates/app.py
+++ templates/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import subprocess
 
 app = Flask(__name__, static_url_path='')
 
@@ -8,9 +9,10 @@ def home():
 
 @app.route("/image/", methods = ['GET', 'POST'])
 def generateImage():
-    # Use model
-    #return request.args.get('prompt')
-    return render_template("index.html")
+    prompt = request.args.get("prompt", default = "student", type = str)
+    fileName = prompt+".png"
+    subprocess.run(["python", "/home/ubuntu/dalle-clip-bias/CLIP+Optim/generate.py", "-p", prompt, "-o", "/home/ubuntu/dalle-clip-bias/templates/static/" + fileName])
+    return render_template("index.html", user_image = fileName)
     
 if __name__ == "__main__":
     app.run(debug=True)
